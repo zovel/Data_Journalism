4c68c494932c529b4c720152c496e4466c78f231
zovel <zoe.a.velie@gmail.com>
2018-12-06 10:47:19 -0600

    added files

---
+++ StarterCode/assets/js/app.js
@@ -0,0 +1,34 @@
+//Define SVG area dimensions
+var svgWidth= 960;
+var svgHeight= 660;
+
+// Define the chart's margins as an object
+var chartMargin = {
+  top: 30,
+  right: 30,
+  bottom: 30,
+  left: 30
+};
+
+// Define dimensions of the chart area
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+// Select body, append SVG area to it, and set the dimensions
+var svg = d3
+  .select("body")
+  .append("svg")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth);
+
+// Append a group to the SVG area and shift ('translate') it to the right and down to adhere
+// to the margins set in the "chartMargin" object.
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
+
+//Import CSV
+d3.csv("data.csv", function(error, data){
+    if (error) return console.warn(error);
+    console.log(data);
+});
